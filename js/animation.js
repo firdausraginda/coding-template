3a100d5c5feda8af238941abfcfae29e32be506e
firdausraginda <ragindafirdaus01@gmail.com>
2019-03-05 01:22:43 +0700

    first commit

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+sr.reveal('.gg-icons', {
+  duration: 600,
+  scale: 0.3,
+  distance: '-150px'
+}, 200);
+sr.reveal('.sr-button', {
+  duration: 1000,
+  delay: 200,
+  distance: '-150px'
+});
+sr.reveal('.sr-contact', {
+  duration: 600,
+  scale: 0.3,
+  distance: '-150px'
+}, 300);
